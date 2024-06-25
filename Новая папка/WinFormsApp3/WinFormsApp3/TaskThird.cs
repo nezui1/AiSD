bc1de2ff3ac35e6dd39c9839dc5dedf4c0881818
nezui1 <lalaevexpress@gmail.com>
2024-06-25 14:23:44 +0300

    5 вариант

---
+++ Новая папка/WinFormsApp3/WinFormsApp3/TaskThird.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp3;
+
+public class TaskThird
+{
+    public void InsertTwo(int[] arr)
+    {
+        int count = 0;
+        for (int i = 0; i < arr.Length / 2 + count; i++)
+        {
+            if (arr[i] % 2 == 0)
+            {
+                count++;
+                for(int j = i + 1; j < arr.Length / 2 + count; j++)
+                {
+                    arr[j + 1] = arr[j];
+                    j++;
+                }
+                arr[i + 1] = 2;
+                i++;
+            }
+        }
+    }
+
+
+}
